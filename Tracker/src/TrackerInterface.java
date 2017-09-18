2ed34a48524f00fce4101805d8d3395a7e1259e7
thenewcomer <1531616544@qq.com>
2017-09-18 14:05:01 +0800

    Tracker_v0.02
    update some comments

--- Tracker/src/TrackerInterface.java
+++ Tracker/src/TrackerInterface.java
@@ -4,17 +4,25 @@ import java.rmi.RemoteException;
 public interface TrackerInterface extends Remote {
     String sayHello() throws RemoteException;
     String returnParameters() throws RemoteException;
+
+    /**
+     * registe a new player
+     * @return registe success or unccess
+     * @throws RemoteException
+     */
     String registeNewPlayer() throws RemoteException;
 
     /**
-     * This function
-     * @return
+     * This function update Tracker's player list
+     * @return success or unsccess
      * @throws RemoteException
      */
-    String updatePlayerList() throws RemoteException;
+    String updatePlayerList(String PlayerList) throws RemoteException;
 
     /**
      * This function return the PlayerList stored in Tracker
+     * @return Tracker's player list
+     * @throws RemoteException
      */
     String getPlayerList() throws RemoteException;
 }
